bff17be93c089ecbbdf0e35e489d172c25de2767
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2021-05-28 16:43:53 +0200

    homework submission

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,9 +7,35 @@ router.get('/:city', async (req, res, next) => {
   try {
     const { city } = req.params;
     const response = await geocoder.geocode(city);
-    console.log(response);
-    res.status(200).send(response);
-    geocoder;
+    const data = response.map((city) => ({
+      ['latitude']: city.latitude,
+      ['longitude']: city.longitude,
+      ['city']: city.city,
+      ['stateCode']: city.stateCode,
+      ['contryCode']: city.countryCode,
+    }));
+    console.log(data);
+    res.status(200).send(data);
+  } catch (error) {
+    next(error);
+    console.log(error);
+  }
+});
+
+router.get('/reverse/:lat/:lon', async (req, res, next) => {
+  try {
+    const { lat, lon } = req.params;
+    const response = await geocoder.reverse({ lat: lat, lon: lon });
+    const data = response.map((city) => ({
+      ['latitude']: city.latitude,
+      ['longitude']: city.longitude,
+      ['city']: city.city,
+      ['stateCode']: city.stateCode,
+      ['contryCode']: city.countryCode,
+    }));
+    console.log(data);
+    // console.log(lat, lon);
+    res.status(200).send(data[0]);
   } catch (error) {
     next(error);
     console.log(error);
@@ -23,6 +49,7 @@ router.get('/weather/:lat/:lon', async (req, res, next) => {
       `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.WEATHER_API_KEY}`
     );
     const data = await respose.json();
+    console.log(data);
     res.status(200).send(data);
   } catch (error) {
     next(error);
