dd23b69f3e598c3475e89487965146f608785a94
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2021-05-28 14:04:15 +0200

    implement get city coordindates and get wheter data from coordinates

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,31 @@
 import { Router } from 'express';
 import geocoder from '../geocoder/index.js';
+import fetch from 'node-fetch';
 const router = Router();
 
-router.get('/', async (req, res) => {
+router.get('/:city', async (req, res, next) => {
   try {
-    res.send('hi');
+    const { city } = req.params;
+    const response = await geocoder.geocode(city);
+    console.log(response);
+    res.status(200).send(response);
+    geocoder;
   } catch (error) {
+    next(error);
     console.log(error);
   }
 });
-router.post('/', async (req, res) => {
+
+router.get('/weather/:lat/:lon', async (req, res, next) => {
   try {
-    const { city } = req.body;
-    const response = await geocoder.geocode(city);
-    console.log(response);
-    res.send('hi');
-    geocoder;
+    const { lat, lon } = req.params;
+    const respose = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.WEATHER_API_KEY}`
+    );
+    const data = await respose.json();
+    res.status(200).send(data);
   } catch (error) {
+    next(error);
     console.log(error);
   }
 });
